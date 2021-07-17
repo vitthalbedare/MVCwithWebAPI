62314d3cc6fa4930222a9d1b74f19582fed7e5a6
Vitthal <vitthal14@gmail.com>
2021-07-17 15:22:23 +0530

    Initial commit

---
+++ MVCwithWebAPI/Models/SqlDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace MVCwithWebAPI.Models
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext() : base("name=SqlConn")
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
